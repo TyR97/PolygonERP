170b349e85e9426bf9900cf52c9b240f6a623f74
TyR97 <vargajanos97@gmail.com>
2025-04-26 19:43:52 +0200

    removed duplicated folders

--- FlaskProject/PolygonERP/app/config/Config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///yourdatabase.db'
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- FlaskProject/PolygonERP/resources/UserResource.py
+++
@@ -1,7 +0,0 @@
-from flask import request, jsonify
-from flask_restful import Resource
-from PolygonERP.models.user import User
-from PolygonERP.models.database import db
-
-class UserResource(Resource):
-    pass
